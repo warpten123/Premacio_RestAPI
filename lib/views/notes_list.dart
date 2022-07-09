b51a97bd0cbfa56fbed6685e98438ff21368b841
Paul Joshua Premacio <paul.premacio.20@usjr.edu.ph>
2022-07-10 00:31:06 +0800

    fetch notes data from API using get http request

--- lib/views/notes_list.dart
+++ lib/views/notes_list.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
+import 'package:premaco_restapi/models/api_response.dart';
 import 'package:premaco_restapi/models/note_for_listing.dart';
 import 'package:premaco_restapi/services/notes_service.dart';
 import 'package:premaco_restapi/views/note_delete.dart';
@@ -15,75 +16,98 @@ class NoteList extends StatefulWidget {
 
 class _NoteListState extends State<NoteList> {
   NotesService get service => GetIt.instance<NotesService>();
-  List<NotesForListing> notes = [];
+
   String formatDateTime(DateTime dateTime) {
     return '${dateTime.day}/${dateTime.month}/${dateTime.year}';
   }
 
+  late APIResponse<List<NotesForListing>> _apiResponse;
+  bool _isLoading = false;
   @override
   void initState() {
-    notes = service.getNoteList();
+    _fetchNotes();
     super.initState();
+  }
+
+  _fetchNotes() async {
+    setState(() {
+      _isLoading = true;
+    });
+
+    _apiResponse = await service.getNoteList();
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('List of notes'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (_) => NoteModify()));
-          },
-          child: const Icon(Icons.add),
-        ),
-        body: ListView.separated(
-          separatorBuilder: (_, Dart_CObject) =>
-              const Divider(height: 1, color: Colors.green),
-          itemCount: notes.length,
-          itemBuilder: (_, index) {
-            return Dismissible(
-              key: ValueKey(notes[index]),
-              direction: DismissDirection.startToEnd,
-              onDismissed: (direction) {},
-              confirmDismiss: (direction) async {
-                final result = await showDialog(
-                    context: context, builder: (_) => NoteDelete());
-                print(result);
-                return result;
-              },
-              background: Container(
-                color: Colors.red,
-                padding: EdgeInsets.only(left: 16),
-                child: Align(
-                  child: Icon(
-                    Icons.delete,
-                    color: Colors.white,
+          appBar: AppBar(
+            title: const Text('List of notes'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (_) => NoteModify()));
+            },
+            child: const Icon(Icons.add),
+          ),
+          body: Builder(builder: (_) {
+            if (_isLoading) {
+              return CircularProgressIndicator();
+            }
+            if (_apiResponse.error) {
+              return Center(
+                child: Text(_apiResponse.errorMessage!),
+              );
+            }
+            return ListView.separated(
+              separatorBuilder: (_, Dart_CObject) =>
+                  const Divider(height: 1, color: Colors.green),
+              itemCount: _apiResponse.data!.length,
+              itemBuilder: (_, index) {
+                return Dismissible(
+                  key: ValueKey(_apiResponse.data![index].noteID),
+                  direction: DismissDirection.startToEnd,
+                  onDismissed: (direction) {},
+                  confirmDismiss: (direction) async {
+                    final result = await showDialog(
+                        context: context, builder: (_) => NoteDelete());
+                    print(result);
+                    return result;
+                  },
+                  background: Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.only(left: 16),
+                    child: Align(
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                      alignment: Alignment.centerLeft,
+                    ),
                   ),
-                  alignment: Alignment.centerLeft,
-                ),
-              ),
-              child: ListTile(
-                onTap: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (_) => NoteModify(
-                            noteID: notes[index].noteID,
-                          )));
-                },
-                title: Text(
-                  notes[index].noteTitle,
-                  style: TextStyle(color: Colors.amber),
-                ),
-                subtitle: Text(
-                    'Last Edited: ${formatDateTime(notes[index].lastEditTime)}'),
-              ),
+                  child: ListTile(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => NoteModify(
+                                noteID: _apiResponse.data![index].noteID,
+                              )));
+                    },
+                    title: Text(
+                      _apiResponse.data![index].noteTitle,
+                      style: TextStyle(color: Colors.amber),
+                    ),
+                    subtitle: Text(
+                        'Last Edited: ${formatDateTime(_apiResponse.data![index].lastEditTime ?? _apiResponse.data![index].createDateTime)}'),
+                  ),
+                );
+              },
             );
-          },
-        ),
-      ),
+          })),
     );
   }
 }
